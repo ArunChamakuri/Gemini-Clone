55348498764c9d3804bf0f3fa7ea843717773a58
ArunChamakuri <arunchamakuri@gmail.com>
2025-10-07 22:00:04 +0530

    Initial commit

---
+++ src/config/gemini.js
@@ -0,0 +1,64 @@
+
+
+
+// Import Gemini SDK
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+}  from "@google/generative-ai";
+
+// Model and API Key
+const MODEL_NAME = "gemini-2.5-flash";
+const API_KEY = "AIzaSyCB072GNtU1J7Jvh7jgLxK11euRVC2ZCb0"; // Replace with your actual API key
+
+async function runChat(prompt) {
+  // Initialize Gemini API
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  // Chat generation settings
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  // Safety settings
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  // Start chat session
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  // Send a new message
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+export default runChat;
